a6aab4e62e97216651ac24aa580c5dd1740fdc40
Vasil <vasilvvmain@gmail.com>
2022-04-12 05:42:59 +0300

    Initialized cmake project.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "config.h"
+
+
+int main()
+{
+  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Version " << Blocks_VERSION_MAJOR << "." << Blocks_VERSION_MINOR << std::endl;
+}
