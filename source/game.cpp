16b61036965d204fa83a46fe9936a78b77c8ed14
Vasil <vasilvvmain@gmail.com>
2022-04-18 06:53:25 +0300

    Added freetype lib.

--- source/game.cpp
+++ source/game.cpp
@@ -62,8 +62,8 @@ Game::~Game()
 int Game::Run()
 {
   glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
