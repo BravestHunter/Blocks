32df32086cf3cfaa490845937b992c6a111ddc18
BravestHunter <vasilvvmain@gmail.com>
2022-05-15 20:31:32 +0300

    Started working on resource tool.

---
+++ tools/ResourceTool/ResourceTool/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ResourceTool.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace ResourceTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Resources.Add("MainViewModel", new MainViewModel());
+
+            //MainWindow.DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ tools/ResourceTool/ResourceTool/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ResourceTool.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceTool.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+    }
+}
